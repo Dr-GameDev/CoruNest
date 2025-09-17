d237d1d35c2084ce1629bf1882ff45ffc88ee23d
Dr-GameDev <raymondbabalo5@gmail.com>
2025-09-17 13:16:25 +0200

    update: containers up and running

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,19 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+// Reuse for registration and logging in.
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+    return view('welcome'); // or any other view you want as the homepage
+})->name('home');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
